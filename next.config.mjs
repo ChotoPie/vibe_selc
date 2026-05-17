1787491793c3d30e8b8d0491f1df7d25464a2e4f
ChotoPie <ChotoPie@users.noreply.github.com>
2026-05-17 17:55:12 +0900

    fix: Vercel 팝업 차단 해결을 위해 COEP(Cross-Origin-Embedder-Policy) 헤더 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,15 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
           },
         ],
       },
